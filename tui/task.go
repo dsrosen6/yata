a6f53dd73bde03cbaf3cfb7a4aeb30dadbcb7a29
Danny Rosenthal <dsrosen6@gmail.com>
2025-12-13 17:43:31 -0600

    wire up db/crud actions to tui

--- tui/task.go
+++ tui/task.go
@@ -4,21 +4,69 @@ import (
 	"context"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
 	"github.com/dsrosen6/yata/models"
 )
 
 type (
-	gotTasksMsg []*models.Task
-	storeErrMsg struct{ err error }
+	dbErrorMsg        struct{ error }
+	refreshTasksMsg   string
+	tasksRefreshedMsg struct{ tasks []*models.Task }
 )
 
-func (m *Model) fetchTasks(ctx context.Context) tea.Cmd {
+func createTaskForm() *huh.Form {
+	return huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Title").
+				Key("title").
+				Inline(true),
+			huh.NewInput().
+				Title("Description").
+				Key("description").
+				Inline(true),
+		),
+	).WithShowHelp(false).WithTheme(huh.ThemeBase())
+}
+
+func (m *Model) insertTask(ctx context.Context, t *models.Task) tea.Cmd {
+	return func() tea.Msg {
+		if _, err := m.repos.Tasks.Create(ctx, t); err != nil {
+			return dbErrorMsg{err}
+		}
+
+		return refreshTasksMsg("refresh")
+	}
+}
+
+func (m *Model) toggleTaskComplete(ctx context.Context, t *models.Task) tea.Cmd {
+	return func() tea.Msg {
+		t.Complete = !t.Complete
+		if _, err := m.repos.Tasks.Update(ctx, t); err != nil {
+			return dbErrorMsg{err}
+		}
+
+		return refreshTasksMsg("refresh")
+	}
+}
+
+func (m *Model) deleteTask(ctx context.Context, id int64) tea.Cmd {
+	return func() tea.Msg {
+		if err := m.repos.Tasks.Delete(ctx, id); err != nil {
+			return dbErrorMsg{err}
+		}
+
+		return refreshTasksMsg("refresh")
+	}
+}
+
+func (m *Model) refreshTasks(ctx context.Context) tea.Cmd {
 	return func() tea.Msg {
 		tasks, err := m.repos.Tasks.ListAll(ctx)
 		if err != nil {
-			return storeErrMsg{err}
+			return dbErrorMsg{err}
 		}
 
-		return gotTasksMsg(tasks)
+		return tasksRefreshedMsg{tasks: tasks}
 	}
 }
--- tui/tui.go
+++ tui/tui.go
@@ -1,70 +1,148 @@
 package tui
 
 import (
+	"context"
 	"fmt"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
 	"github.com/dsrosen6/yata/models"
 )
 
+var ctx = context.Background()
+
 type Model struct {
+	debug bool
 	repos *models.AllRepos
 	tasks []*models.Task
 
-	cursor   int
-	selected map[int]struct{}
+	cursor    int
+	taskMode  taskMode
+	entryForm *huh.Form
+	selected  map[int]struct{}
 }
+
+type taskMode string
+
+const (
+	taskModeViewing  taskMode = "viewing"
+	taskModeCreating taskMode = "creating"
+)
 
 func InitialModel(r *models.AllRepos) *Model {
 	return &Model{
-		repos:    r,
-		tasks:    []*models.Task{},
-		selected: make(map[int]struct{}),
+		repos:     r,
+		tasks:     []*models.Task{},
+		taskMode:  taskModeViewing,
+		entryForm: createTaskForm(),
+		selected:  make(map[int]struct{}),
 	}
 }
 
 func (m *Model) Init() tea.Cmd {
-	// return m.fetchTasks(ctx)
-	return nil
+	return m.refreshTasks(ctx)
 }
 
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
-		case "up", "k":
-			m.cursor = cursorUp(m.cursor, len(m.tasks)-1)
-		case "down", "j":
-			m.cursor = cursorDown(m.cursor, len(m.tasks)-1)
-		case "enter":
-			m.tasks[m.cursor].Complete = !m.tasks[m.cursor].Complete
+	// Universal quit bind
+	if msg, ok := msg.(tea.KeyMsg); ok && msg.String() == "ctrl+c" {
+		return m, tea.Quit
+	}
+
+	switch m.taskMode {
+	case taskModeViewing:
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+			case "ctrl+c", "q":
+				return m, tea.Quit
+			case "up", "k":
+				m.cursor = cursorUp(m.cursor, len(m.tasks)-1)
+			case "down", "j":
+				m.cursor = cursorDown(m.cursor, len(m.tasks)-1)
+			case "enter":
+				if len(m.tasks) > 0 {
+					return m, m.toggleTaskComplete(ctx, m.tasks[m.cursor])
+				}
+			case "d":
+				if len(m.tasks) > 0 {
+					return m, m.deleteTask(ctx, m.tasks[m.cursor].ID)
+				}
+			case "a":
+				m.taskMode = taskModeCreating
+				return m, m.entryForm.Init()
+			}
+
+		case refreshTasksMsg:
+			return m, m.refreshTasks(ctx)
+
+		case tasksRefreshedMsg:
+			m.tasks = msg.tasks
+			return m, nil
 		}
+
+	case taskModeCreating:
+		form, cmd := m.entryForm.Update(msg)
+		if f, ok := form.(*huh.Form); ok {
+			m.entryForm = f
+		}
+
+		if m.entryForm.State == huh.StateCompleted {
+			d := m.entryForm.GetString("description")
+			t := &models.Task{
+				Title:   m.entryForm.GetString("title"),
+				Details: &d,
+			}
+
+			m.tasks = append(m.tasks, t)
+			m.taskMode = taskModeViewing
+			m.entryForm = createTaskForm() // reset the form
+			return m, m.insertTask(ctx, t)
+		}
+		return m, cmd
 	}
 
 	return m, nil
 }
 
 func (m *Model) View() string {
-	s := "Current Tasks:\n"
+	var b strings.Builder
+	b.WriteString(tasksOutput(m.cursor, m.tasks))
 
-	for i, task := range m.tasks {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
-		}
-
-		checked := " "
-		if task.Complete {
-			checked = "x"
-		}
-
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, task.Title)
+	if m.taskMode == taskModeCreating {
+		fmt.Fprintf(&b, "%s\n", m.entryForm.View())
 	}
 
-	s += "\nctrl+c to quit"
-	return s
+	if m.debug {
+		fmt.Fprintf(&b, "Task Mode: %s\nForm State: %v\n", m.taskMode, m.entryForm.State)
+	}
+
+	return b.String()
+}
+
+func tasksOutput(cursor int, tasks []*models.Task) string {
+	if len(tasks) == 0 {
+		return "No tasks found\n"
+	}
+
+	var b strings.Builder
+	b.WriteString("Current Tasks:\n")
+	for i, t := range tasks {
+		cstr := " "
+		if cursor == i {
+			cstr = ">"
+		}
+
+		checked := "󰄱"
+		if t.Complete {
+			checked = "󰄵"
+		}
+		s := fmt.Sprintf("%s %s %s\n", cstr, checked, t.Title)
+		b.WriteString(s)
+	}
+
+	return b.String()
 }
 
 func cursorUp(c, top int) int {
